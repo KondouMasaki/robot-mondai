754c26c51fb61a51a1712986dda4f8b3f83ea1ec
KondouMasaki <48499002+KondouMasaki@users.noreply.github.com>
2021-05-03 23:19:28 +0900

    Add q0-0

---
+++ q0-0/visualflow.js
@@ -0,0 +1,179 @@
+var workspace = Blockly.inject(
+	'blocklyDiv', 
+	{
+		toolbox: Control.prototype.createToolBox(),
+		grid: {
+			spacing: 20,
+			length: 3,
+			color: '#ccc',
+			snap: true,
+		},
+		scrollbars: true,
+		trashcan: true
+	}
+);
+
+var runButton = document.getElementById('runButton');
+var stopButton = document.getElementById('stopButton');
+var robotSpeed = document.getElementById('robotSpeed');
+var myInterpreter = null;
+
+var highlightPause = false;
+var latestCode = '';
+
+var timeoutId = null;
+
+// Load the interpreter now, and upon future changes.
+generateCodeAndLoadIntoInterpreter();
+workspace.addChangeListener(function(event) {
+	if (!(event instanceof Blockly.Events.Ui)) {
+		// Something changed. Parser needs to be reloaded.
+		generateCodeAndLoadIntoInterpreter();
+	}
+});
+
+Control.prototype.initGame();
+runButton.addEventListener("click", runCode, false);
+stopButton.addEventListener("click", forceStop, false);
+stopButton.disabled = true;
+stopButton.setAttribute('class', 'hide');
+
+function initApi(interpreter, globalObject) {
+	// Add an API function for the alert() block, generated for "text_print" blocks.
+	interpreter.setProperty(globalObject, 'alert',
+			interpreter.createNativeFunction(function(text) {
+		text = arguments.length ? text : '';
+	}));
+
+	// Add an API function for the prompt() block.
+	var wrapper = function(text) {
+		return interpreter.createPrimitive(prompt(text));
+	};
+	interpreter.setProperty(globalObject, 'prompt',
+			interpreter.createNativeFunction(wrapper));
+
+	// Add an API function for highlighting blocks.
+	var wrapper = function(id) {
+		id = String(id || '');
+		return interpreter.createPrimitive(highlightBlock(id));
+	};
+	interpreter.setProperty(globalObject, 'highlightBlock',
+			interpreter.createNativeFunction(wrapper));
+	
+	// 
+	var wrapper = function(cmd, arg1, arg2, arg3) {
+		cmd = String(cmd || '');
+		return interpreter.createPrimitive(ControlOneTurn(cmd, arg1, arg2, arg3));
+	};
+	interpreter.setProperty(globalObject, 'ControlOneTurn',
+			interpreter.createNativeFunction(wrapper));
+}
+
+function highlightBlock(id) {
+	workspace.highlightBlock(id);
+	highlightPause = true;
+}
+
+function resetStepUi(clearOutput) {
+	workspace.highlightBlock(null);
+	highlightPause = false;
+}
+
+function generateCodeAndLoadIntoInterpreter() {
+	// Generate JavaScript code and parse it.
+	Blockly.JavaScript.STATEMENT_PREFIX = 'highlightBlock(%1);\n';
+	Blockly.JavaScript.addReservedWords('highlightBlock');
+	latestCode = Blockly.JavaScript.workspaceToCode(workspace);
+	resetStepUi(true);
+}
+
+function stepOneBlock() {
+	highlightPause = false;
+	var hasMoreCode = false;
+	if (myInterpreter instanceof Interpreter) {
+		do {
+			try {
+				hasMoreCode = myInterpreter.step();
+			}
+			finally {
+				if (!hasMoreCode) {
+					stopStep();
+				}
+			}
+		}while(hasMoreCode && !highlightPause);
+	}
+}
+
+function stopStep() {
+	clearInterval(timeoutId);
+	myInterpreter = null;
+	resetStepUi(false);
+	setTimeout(function() {
+		runButton.disabled = false;
+		runButton.setAttribute('class', '');
+		stopButton.disabled = true;
+		stopButton.setAttribute('class', 'hide');
+		robotSpeed.setAttribute('class', '');
+		Control.prototype.balloon.setAttribute('class', '');
+	}, 1000);
+}
+
+function runCode() {
+	Control.prototype.beforeRun();
+	
+	if (!myInterpreter) {
+		// DBG
+		//alert(latestCode);
+		
+		runButton.disabled = true;
+		runButton.setAttribute('class', 'hide');
+		stopButton.disabled = false;
+		stopButton.setAttribute('class', '');
+		robotSpeed.setAttribute('class', 'hide');
+
+		Control.prototype.balloon.setAttribute('class', 'hide');
+		
+		resetStepUi(true);
+		myInterpreter = new Interpreter(latestCode, initApi);
+
+		// DBG
+		//alert(latestCode);
+		
+		var spd = 1000;
+		switch(parseInt(robotSpeed.value)) {
+			case 0:
+				spd = 1500;
+				break;
+			case 2:
+				spd = 700;
+				break;
+			default:
+				break;
+		}
+
+		highlightPause = true;
+		timeoutId = setInterval(stepOneBlock, spd);
+	}
+}
+
+function ControlOneTurn(cmd, arg1, arg2, arg3) {
+	var ret = Control.prototype.oneTurn(cmd, arg1, arg2, arg3);
+	
+	if (Control.prototype.goal) {
+		stopStep();
+		alert("やったー！ゴールに着いたよ！");
+		return ret;
+	}
+	
+	if (Control.prototype.emptyLife) {
+		stopStep();
+		alert("命令が多くて、\nつかれちゃった……");
+		return ret;
+	}
+	
+	return ret;
+}
+
+function forceStop() {
+	stopStep();
+}
