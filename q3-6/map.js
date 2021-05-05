32324e55e38b0fd15ff7c26c1decd21fe6aa8185
KondouMasaki <48499002+KondouMasaki@users.noreply.github.com>
2021-05-05 13:16:44 +0900

    Add Q3-4,3-5,3-6

---
+++ q3-6/map.js
@@ -0,0 +1,113 @@
+var Map = function() {
+};
+Map.prototype = {
+	"map": [
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,0,0,1,1,1,1,1],
+		[1,1,1,1,1,0,0,1,1,1,1,1],
+		[1,1,1,1,1,0,0,1,1,1,1,1],
+		[1,1,1,1,1,0,0,1,1,1,1,1],
+		[1,1,1,1,1,0,0,1,1,1,1,1],
+		[1,1,1,1,1,0,0,1,1,1,1,1],
+		[1,1,1,1,1,0,0,1,1,1,1,1],
+		[1,1,1,1,1,0,0,1,1,1,1,1],
+		[1,1,1,1,1,0,0,1,1,1,1,1],
+		[1,1,1,1,1,0,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1]
+	],
+	"start": {
+		"x": 5,
+		"y": 10,
+		"direction": 0,
+		"life": 65534,
+	},
+	"hint": "赤いマスの右にゴールがあるよ",
+	"state": 0,
+	"goals": 1,
+	"links": {
+		"question": "Q3-6",
+		"previous": "q3-5",
+		"next": ""
+	},
+	"robot": {
+		"type": 2,
+		"Basic": {
+			"forward": true,
+			"turn_right": true,
+			"turn_left": true,
+			"nop": true
+		},
+		"Standard": {
+			"floor_color_is": false,
+			"robot_direction_is": true,
+			"movable_is": true
+		},
+		"Advanced": {
+			"times_loop": false,
+			"floor_color_loop": true,
+			"movable_loop": false
+		},
+		"Expert": {
+			"write_register": true,
+			"read_register": true,
+			"get_floor_color": true,
+			"get_direction": true
+		},
+		"Enhanced": {
+			"values_equal_is": true,
+			"values_equal_loop": true,
+			"infinity_loop": true,
+			"is_movable_to": true
+		},
+		"Superior": {
+			"add_register": true,
+			"sub_register": true,
+			"add_register_index": true,
+			"sub_register_index": true,
+			"set_register_index": true,
+			"get_register_index": true
+		},
+		"Replete": {
+			"read_cell_value": true,
+			"read_cell_value_index": true,
+			"write_cell_value": true,
+			"values_compare": true,
+			"expression_if": true,
+			"expression_loop": true
+		},
+		"Master": {
+		}
+	},
+	"chars": [
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ]
+	],
+	"map2": [],
+	"chars2": []
+};
+
+/**
+ * コード実行前の処理
+ */
+Map.prototype.beforeStart = function() {
+	var v = parseInt(Math.random() * 8) + 2;
+	Map.prototype.map[v - 1][5] = 2;
+	Map.prototype.map[v - 1][6] = 5;
+	Map.prototype.map[v - 2][6] = 1;
+	Map.prototype.map[v][6] = 1;
+};
+/**
+ * ターンごとに発生する処理
+ */
+Map.prototype.afterMoved = function(t, pos) {
+};
